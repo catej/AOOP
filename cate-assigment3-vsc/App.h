ac7eae69073e8b5e493632e0e57a5733330c5d61
Jeff A Cate <Jeff.A.Cate@gmail.com>
2021-02-01 18:19:31 -0600

    Setup assignment 3

---
+++ cate-assigment3-vsc/App.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class App {
+    public:
+        void Run()
+        {
+            cout << "Run()" << endl;
+        }
+
+};
--- cate-assigment3-vsc/program.cpp
+++ cate-assigment3-vsc/program.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "Student.h"
+#include "App.h"
 
 using namespace std;
 
@@ -11,42 +11,10 @@ using namespace std;
 *   ASSIGNMENT # : 3
 */
 
-Student GetStudent()
-{
-    string name;
-    int age;
-    double gpa;
-
-    cout << "Please enter your name: ";
-    cin >> name;
-
-    cout << "Please enter your age(int): ";
-    cin >> age;
-
-    cout << "Please enter your gpa(double): ";
-    cin >> gpa;
-
-    Student student = Student(name, age, gpa);
-    return student;
-}
-
 int main()
 {
-    cout << "Assignment 3 \n"
-            "------------\n"
-            "1) Create a class\n"
-            "2) Instantiate objects\n"
-            "3) Add objects to vector\n"
-            "4) Create static class to display vector\n\n" ;
-
-    Student student1 = Student("Ron", 70, 4.0);
-    Student student2 = Student("Curtis", 65, 2.698);
-    vector<Student> students{student1, student2};
-    students.push_back(GetStudent());
-    for(Student student : students){
-        student.Display();
-    }
-
+    App application = App();
+    application.Run();
 
     getchar();
     return 0;
