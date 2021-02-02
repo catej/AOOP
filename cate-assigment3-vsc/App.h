093c5d28b35e8fa52b156b0fcf87a29e58a93c0d
Jeff A Cate <Jeff.A.Cate@gmail.com>
2021-02-01 19:54:25 -0600

    Menu created

--- cate-assigment3-vsc/App.h
+++ cate-assigment3-vsc/App.h
@@ -1,14 +1,43 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
+#include "Order.h"
 
 using namespace std;
+
+// shipping 10/spool;
+// cost = 100/spool;
 
 class App {
     public:
         void Run()
         {
-            cout << "Run()" << endl;
+            Menu();
+            TakeOrder();
         }
+
+    private:
+    string stockPath = "Inventory.txt";
+
+    void Menu()
+    {
+        cout << "Welcome to Wires\'R\'US\n" << "---------------------\n\n";
+        cout << "Spools Available: " << GetStock() << "\n";
+    }
+    int GetStock()
+    {
+        fstream stockFile(stockPath);
+        int stock;
+        stockFile >> stock;
+        stockFile.close();
+        return stock;
+    }
+    void TakeOrder()
+    {
+        int spoolsToOrder;
+        cout << "Spools to order: ";
+        cin >> spoolsToOrder;
+    }
 
 };
