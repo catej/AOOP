c2c78bcba6e43b6b74e0108eee38b197869905e2
Jeff A Cate <Jeff.A.Cate@gmail.com>
2021-02-12 23:43:33 -0600

    Game setup complete.

--- cate-assignment4-vsc/Game.h
+++ cate-assignment4-vsc/Game.h
@@ -8,12 +8,19 @@ class Game {
     private:
         const string HEADS = "HEADS";
         const string tails = "tails";
+        bool gameOver;
+        double playerBalance;
         Coin quarter;
         Coin dime;
         Coin nickel;
+
         
     public:
         Game(){
-
+            quarter = Coin(.25);
+            dime = Coin(.10);
+            nickel = Coin(.05);
+            playerBalance = 0;
+            gameOver = false;
         }
 };
