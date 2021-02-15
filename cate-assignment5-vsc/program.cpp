c72d94ca960ba56265f451e24d2bc19c727d505f
Jeff A Cate <Jeff.A.Cate@gmail.com>
2021-02-14 22:05:52 -0600

    Reel tested && code clean up.

--- cate-assignment5-vsc/program.cpp
+++ cate-assignment5-vsc/program.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Reel.h"
 
 using namespace std;
 
@@ -12,15 +11,6 @@ using namespace std;
 int main() {
 
     srand(time(0));
-
-    Reel reel = Reel();
-    cout << reel.getFace() << "\n";
-
-    for (int  i = 0; i < 10; i++)
-    {
-        reel.spin();
-        cout << reel.getFace() << "\n";
-    }
 
     getchar();
     return 0;
