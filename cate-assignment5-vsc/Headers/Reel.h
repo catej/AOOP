21ce3004739fcadc5453de3e56ef478c870e7174
Jeff A Cate <Jeff.A.Cate@gmail.com>
2021-02-14 21:38:26 -0600

    Updated template project to include student information and rng helper.

---
+++ cate-assignment5-vsc/Headers/Reel.h
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+class Reel {
+    public:
+
+    private:
+        string faces[20]= {
+            "   LEMON       ",
+            "   CHERRY      ",
+            "   BANANA      ",
+            "   SEVEN       ",
+            "   DIAMOND     ",
+            "   BELL        ",
+            "   HEART       ",
+            "   STAR        ",
+            "   STRAWBERRY  ",
+            "   GRAPE       ",
+            "   BELL        ",
+            "   HORSESHOE   ",
+            "   HEART       ",
+            "   PEAR        ",
+            "   ORANGE      ",
+            "   TEN         ",
+            "   KING        ",
+            "   QUEEN       ",
+            "   JACK        ",
+            "   JOKER       "};
+};
--- cate-assignment5-vsc/program.cpp
+++ cate-assignment5-vsc/program.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
+/*
+*        STUDENT : Jeff Cate
+*       COURSE # : ITDEV185-900
+*   ASSIGNMENT # : 5 - Slot Machine
+*/
+
 int main() {
 
-    
+    srand(time(0));
 
     getchar();
     return 0;
