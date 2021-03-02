d6bf1fbfb20c3c6a6e8b6b9a9d162d3d7eecd1d3
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-01 23:31:23 -0600

    Coin header set up as base class.

--- Cate_Assignment7/Cate_Assignment7/Coin.h
+++ Cate_Assignment7/Cate_Assignment7/Coin.h
@@ -1,8 +1,25 @@
 #ifndef Coin_H
 #define Coin_H
+#include <string>
+
+using namespace std;
+
 class Coin
 {
-
+	private:
+		static double balance;
+		string sideUp;
+		bool heads;
+		double value;
+		const string heads = "HEADS";
+		const string tails = "tails";
+	public:
+		Coin(double v);
+		void addBalance();
+		static double getBalance();
+		void toss();
+		bool getHeads();
+		string getSideUp();
 };
 #endif
 
