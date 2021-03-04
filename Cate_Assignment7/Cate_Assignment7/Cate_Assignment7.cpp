64fcd25a57df7d93762ea6638c0b138888bb2b63
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-03 18:02:26 -0600

    comments added per assignment req.

--- Cate_Assignment7/Cate_Assignment7/Cate_Assignment7.cpp
+++ Cate_Assignment7/Cate_Assignment7/Cate_Assignment7.cpp
@@ -1,5 +1,6 @@
 #include "Cate_Assignment7.h"
 #include <ctime>
+#include <iomanip>
 #include <iostream>
 #include "Coin.h"
 #include "Quarter.h"
@@ -16,11 +17,14 @@ using namespace std;
 
 double Coin::balance = 0;
 
+// plays each round: tossCoin => checkHeads => addBalance
 void evaluateToss(Coin& coin) {
 	coin.toss();
 	if (coin.getHeads()) {
 		coin.addBalance();
 	}
+	// displays data after each flip
+	cout << fixed << setprecision(2) << (coin.getHeads() == 0 ? "false" : "true ") << " : " << coin.getValue() << " | balance: " << Coin::getBalance() << endl;
 }
 
 int main() {
@@ -29,6 +33,7 @@ int main() {
 
 	bool gameOver = false;
 
+	// initialize coins
 	Coin quarter = Quarter();
 	Coin dime = Dime();
 	Coin nickel = Nickel();
@@ -38,14 +43,16 @@ int main() {
 		evaluateToss(dime);
 		evaluateToss(nickel);
 
+		//check if the game is over
 		if (Coin::getBalance() >= 1) {
 			gameOver = true;
 		}
 	}
 
-	cout << "You "
-		<< (Coin::getBalance() == 1 ? "won!!" : "lost.") << endl
-		<< "balance: " << Coin::getBalance() << endl;
+	// display results of the game
+	cout << "\nYou "
+		 << (Coin::getBalance() == 1 ? "won!!" : "lost.") << endl
+		 << "balance: " << Coin::getBalance() << endl;
 
 	char end = getchar();
 	return 0;
