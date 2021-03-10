f2a4fc9e61dbb63ae550f9b94851028c348f70e1
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-09 21:31:15 -0600

    Main Menu complete.

--- Cate_Midterm/Cate_Midterm/Cate_Midterm.cpp
+++ Cate_Midterm/Cate_Midterm/Cate_Midterm.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include "Item.h"
+#include <vector>
 
 /*
 *        STUDENT : Jeff Cate
@@ -10,9 +11,39 @@
 
 double Item::delivery = 0;
 double Item::tip = 0;
+vector<Item*> cart;
+
+void displayMainMenu() {
+
+	int choice = -1;
+
+	while (choice < 0 || choice > 3) {
+		cout << "(1) Pick up\n"
+			 << "(2) Delivery\n"
+			 << "(3) Exit\n"
+			 << "Your selection: ";
+		
+		cin >> choice; 
+
+		if (choice < 0 || choice > 3) {
+			cout << "Invaid selection! Please try again.\n";
+		}
+	}
+
+	if (choice == 2) {
+		Item::delivery = 20;
+		Item::tip = 5;
+	}
+}
 
 int main()
 {
+	displayMainMenu();
+	
+	cout << "Tip: " << Item::tip << "\n"
+		 << "Delivery: " << Item::delivery << "\n";
+	
 	char end = getchar();
+	end = getchar();
 	return 0; 
 }
