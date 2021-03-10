d371f41ddfc645fe9d413371890e087ea2e407eb
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-10 12:46:19 -0600

    Added meat menu

--- Cate_Midterm/Cate_Midterm/Cate_Midterm.cpp
+++ Cate_Midterm/Cate_Midterm/Cate_Midterm.cpp
@@ -15,13 +15,16 @@ vector<Item*> cart;
 
 void displayDeliveryMenu() {
 
-	int choice = -1;
+	system("cls");
 
+	int choice = -1;
+	
 	while (choice < 1 || choice > 3) {
-		system("cls");
-		cout << "(1) Pick up\n"
+		cout << "-- Delivery Menu --\n"
+			 << "(1) Pick up\n"
 			 << "(2) Delivery\n"
 			 << "(3) Exit\n"
+			 << "--------------------\n"
 			 << "Your selection: ";
 		
 		cin >> choice; 
@@ -44,19 +47,44 @@ void displayFresh() {
 	int choice = -1;
 
 	while (choice != 4) {
-		cout << "(1) Gala Apples    $3.99/lb\n"
+		cout << "-------- Fresh Menu -------\n" 
+			 << "(1) Gala Apples    $3.99/lb\n"
 			 << "(2) Banana         $0.48/lb\n"
 			 << "(3) Grapes         $2.99/lb\n"
 			 << "(4) Return to Main Menu\n"
+			 << "--------------------\n"
 			 << "Your selection: ";
 
 		cin >> choice;
 
+		system("cls");
+		
 		if (choice < 1 || choice > 4) {
 			cout << "Invaid selection! Please try again.\n";
 		}
+	}
+}
+
+void displayMeat() {
+	system("cls");
+	int choice = -1;
+
+	while (choice != 4) {
+		cout << "-------- Meat Menu -------\n"
+			<< "(1) Whole Chicken  $6.99/lb\n"
+			<< "(2) Ground beef    $4.99/lb\n"
+			<< "(3) Salmon         $9.99/lb\n"
+			<< "(4) Return to Main Menu\n"
+			<< "--------------------\n"
+			<< "Your selection: ";
+
+		cin >> choice;
 
 		system("cls");
+
+		if (choice < 1 || choice > 4) {
+			cout << "Invaid selection! Please try again.\n";
+		}
 	}
 }
 
@@ -65,10 +93,12 @@ void displayMainMenu() {
 	int choice = -1;
 
 	while (choice != 4) {
-		cout << "(1) Fresh Produce\n"
+		cout << "----- Main Menu ----\n" 
+			 << "(1) Fresh Produce\n"
 			 << "(2) Meat and Seafood\n"
 			 << "(3) Frozen Foods\n"
 			 << "(4) Check Out\n"
+			 << "--------------------\n"
 			 << "Your selection: ";
 		
 		cin >> choice;
