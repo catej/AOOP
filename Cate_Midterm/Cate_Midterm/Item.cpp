f4eb815d1b0e54cf15d7e7276ce29bf5b2da4209
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-10 13:59:44 -0600

    Static member functions implemented.

--- Cate_Midterm/Cate_Midterm/Item.cpp
+++ Cate_Midterm/Cate_Midterm/Item.cpp
@@ -1,1 +1,17 @@
 #include "Item.h"
+
+double Item::getTip() {
+	return tip;
+}
+
+void Item::setTip(double tip) {
+	Item::tip = tip;
+}
+
+double Item::getDelivery() {
+	return delivery;
+}
+
+void Item::setDelivery(double delivery) {
+	Item::delivery = delivery;
+}
