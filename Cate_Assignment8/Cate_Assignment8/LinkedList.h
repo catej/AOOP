411f94aa70ffdcd782ff7f74940843fc5d5291c1
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-15 16:34:58 -0500

    Assignment 8: Defined.

--- Cate_Assignment8/Cate_Assignment8/LinkedList.h
+++ Cate_Assignment8/Cate_Assignment8/LinkedList.h
@@ -3,5 +3,17 @@
 
 class LinkedList
 {
+	private:
+		PersonNode* headPtr;
+		PersonNode* tailPtr;
+
+	public:
+		LinkedList();
+		void addLink();
+		void findInsertSpot();
+		void getHeadPtr();
+		void displayList();
+		void displayPersonNode();
+
 };
 
