b95a0daf8f3d5d7195ff31678a7593add1088523
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-22 23:17:23 -0500

    Project initialized.

---
+++ Cate_Assignment9/Cate_Assignment9/Cate_Assignment9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+*        STUDENT : Jeff Cate
+*       COURSE # : ITDEV185-900
+*   ASSIGNMENT # : 9 - The Presentation (Abstract Class, Virtual fulction, Polymorphism)
+*/
+
+
+int main()
+{
+    cout << "Welcome to the Area Calculator" << endl;
+
+    char end = getchar();
+}
