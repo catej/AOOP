e67723b32744bf90cef58efddc593a6c64aa97db
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-28 11:34:55 -0500

    Defined contact setters.

--- Cate_Assignment10/Cate_Assignment10/Contact.cpp
+++ Cate_Assignment10/Cate_Assignment10/Contact.cpp
@@ -1,1 +1,14 @@
 #include "Contact.h"
+
+void Contact::setFirstName(string firstName) {
+
+}
+void Contact::setFullName(string fullName) {
+
+}
+void Contact::setLastName(string lastName) {
+
+}
+void Contact::setPhone(string phone) {
+
+}
