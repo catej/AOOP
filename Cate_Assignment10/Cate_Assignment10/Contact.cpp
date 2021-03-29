99f068b252367011b8b773b94d27adc1638a0938
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-28 20:21:09 -0500

    Contact Getter/Setters complete (untested).

--- Cate_Assignment10/Cate_Assignment10/Contact.cpp
+++ Cate_Assignment10/Cate_Assignment10/Contact.cpp
@@ -5,33 +5,33 @@ Contact::Contact() {
 }
 
 void Contact::setFirstName(string firstName) {
-
+	strcpy(this->firstName, firstName.c_str());
 }
 
 char* Contact::getFirstName(){
-
+	return firstName;
 }
 
 void Contact::setFullName(string fullName) {
-
+	strcpy(this->fullName, fullName.c_str());
 }
 
 char* Contact::getFullName() {
-
+	return fullName;
 }
 
 void Contact::setLastName(string lastName) {
-
+	strcpy(this->lastName, lastName.c_str());
 }
 
 char* Contact::getLastName() {
-
+	return lastName;
 }
 
 void Contact::setPhone(string phone) {
-
+	strcpy(this->phone, phone.c_str());
 }
 
 char* Contact::getPhone() {
-
+	return phone;
 }
