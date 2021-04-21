1b1ad1f113ea29c21eb4b3fc124714c4a7a3b209
JeffACate <Jeff.A.Cate@gmail.com>
2021-04-20 19:18:22 -0500

    setup assignment-12

---
+++ Cate_Assignment12/Cate_Assignment12/PersonNode.cpp
@@ -0,0 +1,49 @@
+#include "PersonNode.h"
+
+PersonNode::PersonNode() {
+	address = "";
+	fName = "";
+	lName = "";
+	PersonNode* next = NULL;
+	phone = "";
+}
+
+void PersonNode::setFName(string fName) {
+	this->fName = fName;
+}
+
+void PersonNode::setLName(string lName) {
+	this->lName = lName;
+}
+
+string PersonNode::getLName() {
+	return lName;
+}
+
+string PersonNode::getFullName() {
+	return fName + " " + lName;
+}
+
+void PersonNode::setPhone(string phone) {
+	this->phone = phone;
+}
+
+string PersonNode::getPhone() {
+	return phone;
+}
+
+void PersonNode::setAddress(string address) {
+	this->address = address;
+}
+
+string PersonNode::getAddress() {
+	return address;
+}
+
+void PersonNode::setNext(PersonNode* next) {
+	this->next = next;
+}
+
+PersonNode* PersonNode::getNext() {
+	return next;
+}
